f5b409666694c596bc181fdd228a75b4110ed769
Lars M <lars.musaus@gmail.com>
2020-10-20 23:46:42 +0200

    new site with most of the functions, miss about section

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'ok'
+	return render_template('index.html')
+
+@app.route('/test')
+def test():
+	return 'Test works!'
 
 if __name__ == "__main__":
 	app.run()
-
