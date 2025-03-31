d193569d40a17056f78bfe44272a9642250c5cab
jkaob <j.grepperud@gmail.com>
2025-03-31 23:12:53 +0200

    fix update_csv action for workflows

--- ball25.py
+++ ball25.py
@@ -1,7 +1,10 @@
 import data.tips25
 from tippedata import TippeData
 from common import Team
-
+import os
+import csv
+import datetime
+import shutil
 
 class TippeData25(TippeData):
     def __init__(self, debug=False):
@@ -26,6 +29,47 @@ class TippeData25(TippeData):
             Team('Åsane', 'ÅSA'),
         ]
         super().__init__(year, entries, teams, debug)
+
+    @staticmethod
+    def action_update_csv(year, dir_prefix=None, backup_only=True):
+        if (dir_prefix is None):
+            dir_prefix = os.getcwd()
+
+        ball = TippeData25(year)
+
+        now = datetime.datetime.now()
+
+        backup_dir = f"{dir_prefix}/data/backup/time"  # Adjust the directory path
+        os.makedirs(backup_dir, exist_ok=True)  # Ensure the backup directory exists
+        backup_time = f"{backup_dir}/{year}-{now.month:02d}-{now.day:02d}-{now.hour:02d}.{now.minute:02d}.csv"
+
+        if backup_only:
+            ball.update_csv(output_fname=backup_time)
+            return
+
+        # update main CSV file here
+        updated_pos, updated_matches = ball.update_csv()
+
+        if updated_matches or updated_pos:
+            # make backup whenever something has changed
+            shutil.copy(ball.reader.csv, backup_time)
+            print(f"\nBackup file at {backup_time}")
+
+            # Hotfix - use full path to ensure stuff is saved
+            full_csv_path = f"{dir_prefix}/data/{year}.csv"
+            try:
+                shutil.copy(ball.reader.csv, full_csv_path)
+                print(f"\nSaved CSV file at {full_csv_path}")
+            except:
+                print(f"\nFile should be found at {full_csv_path}")
+                pass
+
+        if updated_pos:
+            num = ball.reader.get_n_pos_rows_written()
+            backup = f"{dir_prefix}/data/backup/{year}-r{num}.csv"
+            shutil.copy(ball.reader.csv, backup)
+            print(f"\nBackup file at {backup}")
+
 
 
 
