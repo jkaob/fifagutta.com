0b949e552e86be83ed37739e78cb9a06004a28e8
jakob <jakob@hyke.no>
2025-05-05 16:17:12 +0200

    Add .env file locally with secret stuff and load it

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
-from ball24 import TippeData24
-from ball25 import TippeData25
-#from ball23 import TippeDataBase
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify, session
+from src.ball24 import TippeData24
+from src.ball25 import TippeData25
+from src.kampspill import Kampspill
+import os
+import json
 
 app = Flask(__name__)
+
+load_dotenv() # Load environment variables from .env file
+app.secret_key = os.getenv('SECRET_KEY')
+VALID_PASSWORDS = json.loads(os.getenv('PASSWORDS_JSON'))
+
 
 @app.route('/')
 def index():
@@ -39,16 +47,31 @@ def last_year():
         contestants_json=contestants_json
     )
 
+@app.route('/matches')
+def match_bets():
+	kampspill = Kampspill(2025)
+	next_matches = kampspill.get_next_matches(7)
+	return render_template(
+		'kampspill.html',
+		next_matches=next_matches)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    pw = request.json.get('password')
+    username = VALID_PASSWORDS.get(pw)
+    if username:
+        session['username'] = username
+        return jsonify({'success': True, 'username': username})
+    else:
+        return jsonify({'success': False}), 401
+
 
 
 
 @app.route('/2023')
 def hjelp():
 	return "vi rykket ned"
-
-@app.route('/test')
-def test():
-	return 'Test works!'
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
