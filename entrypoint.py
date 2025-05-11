bd8ca3dd44c0dc09b032544c0cfa01e8289070e8
jakob <jakob@hyke.no>
2025-05-11 16:03:17 +0200

    Add 'csv' arg to entrypoint.py - update workflow

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,17 @@
 from  src.ball25 import TippeData25
 import os
+import sys
 
 YEAR=2025
 
 
-TippeData25.action_update_csv(dir_prefix=os.getenv('GITHUB_WORKSPACE'), backup_only=False)
+if __name__ == "__main__":
+
+    arg = sys.argv[1] if len(sys.argv) > 1 else ""
+    if arg == "csv":
+        print("updating csv")
+        TippeData25.action_update_csv(dir_prefix=os.getenv('GITHUB_WORKSPACE'), backup_only=False)
+
+    else: 
+        print("updating match schedule")
+        # other stuff
